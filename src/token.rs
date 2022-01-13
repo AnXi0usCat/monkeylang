07650a8f0fbdfb852c0b4aad81a4ff0375760e9c
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-01-13 23:25:36 +0000

    create tokens and make them printable by implementing Display

---
+++ src/token.rs
@@ -0,0 +1,56 @@
+use std::fmt;
+use std::fmt::{Formatter, write};
+
+enum Token {
+    Illegal,
+    Eof,
+
+    // Identifiers + literals
+    Ident(String),
+    Int(String),
+
+    // Operators
+    Assign,
+    Plus,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+
+    // Keywords
+    Let,
+    Function
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match &self {
+            Token::Illegal => write!(f, "ILLEGAL"),
+            Token::Eof => write!(f, "EOF"),
+            Token::Ident(ident) => write!(f, "{}", ident),
+            Token::Int(int) => write!(f, "{}", int),
+            Token::Assign => write!(f, "="),
+            Token::Plus => write!(f, "+"),
+            Token::Comma => write!(f, ","),
+            Token::Semicolon => write!(f, ";"),
+            Token::Lparen => write!(f, "("),
+            Token::Rparen => write!(f, ")"),
+            Token::Lbrace => write!(f, "{{"),
+            Token::Rbrace => write!(f, "}}"),
+            Token::Let => write!(f, "LET"),
+            Token::Function => write!(f, "FUNCTION")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
