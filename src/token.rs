108c5c320f7882d2c46cc7a2fc6c5a98f02557a7
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-01-23 16:02:22 +0000

    add two char tokens

--- src/token.rs
+++ src/token.rs
@@ -13,6 +13,16 @@ pub enum Token {
     // Operators
     Assign,
     Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    Gthen,
+    Lthen,
+    Equals,
+    Gequals,
+    Lequals,
+    Nequals,
 
     // Delimiters
     Comma,
@@ -24,18 +34,33 @@ pub enum Token {
 
     // Keywords
     Let,
-    Function
+    Function,
+    If,
+    Else,
+    Return,
+    True,
+    False
 }
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match &self {
+        match self {
             Token::Illegal => write!(f, "ILLEGAL"),
             Token::Eof => write!(f, "EOF"),
             Token::Ident(ident) => write!(f, "{}", ident),
             Token::Int(int) => write!(f, "{}", int),
             Token::Assign => write!(f, "="),
             Token::Plus => write!(f, "+"),
+            Token::Minus => write!(f, "-"),
+            Token::Bang => write!(f, "!"),
+            Token::Asterisk => write!(f, "*"),
+            Token::Slash => write!(f, "/"),
+            Token::Gthen => write!(f, ">"),
+            Token::Lthen => write!(f, "<"),
+            Token::Equals => write!(f, "=="),
+            Token::Gequals => write!(f, ">="),
+            Token::Lequals => write!(f, "<="),
+            Token::Nequals => write!(f, "!="),
             Token::Comma => write!(f, ","),
             Token::Semicolon => write!(f, ";"),
             Token::Lparen => write!(f, "("),
@@ -43,7 +68,12 @@ impl fmt::Display for Token {
             Token::Lbrace => write!(f, "{{"),
             Token::Rbrace => write!(f, "}}"),
             Token::Let => write!(f, "LET"),
-            Token::Function => write!(f, "FUNCTION")
+            Token::Function => write!(f, "FUNCTION"),
+            Token::If => write!(f, "if"),
+            Token::Else => write!(f, "else"),
+            Token::Return => write!(f, "return"),
+            Token::True => write!(f, "true"),
+            Token::False => write!(f, "false")
         }
     }
 }
@@ -52,6 +82,11 @@ pub fn lookup_identifier(ident: &str) -> Token {
     match ident {
         "fn" => Token::Function,
         "let" => Token::Let,
+        "if" => Token::If,
+        "else" => Token::Else,
+        "return" => Token::Return,
+        "true" => Token::True,
+        "false" => Token::False,
         _ => Token::Ident(String::from(ident))
     }
 }
