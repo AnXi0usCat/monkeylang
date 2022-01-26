f19c6fd4c6a86210304023b813d017d78a403079
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-01-26 19:11:17 +0000

    iterate over lexer in repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,7 +1,6 @@
 use std::{io, process};
 use std::io::BufRead;
 use crate::lexer::Lexer;
-use crate::token::Token;
 
 pub fn start() {
 
@@ -9,21 +8,16 @@ pub fn start() {
 
     loop {
         let mut input =  String::new();
-        print!("{}", ">>");
+        print!("{}", ">> ");
 
         stdin.lock().read_line(&mut input).unwrap_or_else(|err| {
             eprintln!("Problem reading input: {:?}", (err));
             process::exit(1);
         });
 
-        let mut lexer = Lexer::new(&input);
-
-        while let token = lexer.next_token() {
-            if token == Token::Eof {
-                process::exit(0);
-            }
-            print!("{}", token);
+        let lexer = Lexer::new(&input);
+        for token in lexer {
+            println!("{}", token);
         }
-
     }
 }
