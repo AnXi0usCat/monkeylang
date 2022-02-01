1331d64404bbef64d58b3dd695475cdab89ba019
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-02-01 20:30:18 +0000

    initial commit for the AST whith program, experssion and statement

---
+++ src/ast.rs
@@ -0,0 +1,44 @@
+use std::fmt;
+use std::fmt::Formatter;
+
+#[derive(Debug, PartialEq)]
+pub struct Program {
+    statements: Vec<Statement>
+}
+
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for statement in &self.statements {
+            write!(f, "{}", statement);
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Statement {
+    Let(String, Expression)
+}
+
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Let(name, value) => write!(f, "{} = {}", name, value),
+        };
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Expression {
+    Identifier(String)
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self{
+            Self::Identifier(value) => write!(f, "{}", value),
+        };
+        Ok(())
+    }
+}
