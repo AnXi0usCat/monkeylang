45a6426e497c0817522b503ea54eca5aa710eb42
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-02-03 22:35:36 +0000

    add parse_rpgram and parse_statement methods

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
-use std::mem;
+use std::{io, mem};
 use crate::lexer::Lexer;
 use crate::token::Token;
+use crate::ast::{Program, Statement};
 
 pub struct Parser<'a> {
     lexer: Lexer<'a>,
@@ -24,5 +25,41 @@ impl<'a> Parser<'a> {
         self.cur_token = mem::replace(
             &mut self.peek_token, self.lexer.next_token()
         )
+    }
+
+    fn parse_program(&mut self) -> Program {
+        let mut statements = vec![];
+
+        while self.cur_token != Token::Eof {
+            match self.parse_statement() {
+                Ok(res) => statements.push(res),
+                Err(err) => panic!("failed to parse statement {}", err)
+            }
+            self.next_token();
+        }
+
+        Program { statements }
+    }
+    
+    fn parse_statement(&mut self) -> Result<Statement, E> {
+        match self.cur_token {
+            Token::Let => self.parse_let_statement(),
+            _ => Err("")
+        }
+    }
+
+    fn parse_let_statement(&mut self) -> Statement::Let {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::Lexer;
+    use crate::parser::Parser;
+
+    #[text]
+    fn let_statement() {
+
     }
 }
