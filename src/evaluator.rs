b3ec988669c578414e5cb7cd1047e4725f71aca0
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-04-14 21:54:15 +0100

    evaluete infix expressions with integers

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,4 +1,4 @@
-use crate::ast::{Expression, Prefix, Program, Statement};
+use crate::ast::{Expression, Infix, Prefix, Program, Statement};
 use crate::object::Object;
 
 pub fn eval(program: &Program) -> Result<Object, String> {
@@ -19,6 +19,11 @@ fn eval_expression(expr: &Expression) -> Result<Object, String> {
         Expression::IntegerLiteral(value) => Ok(Object::Integer(*value)),
         Expression::Boolean(value) => Ok(Object::Boolean(*value)),
         Expression::PrefixExpression(prefix, expr) => eval_prefix_expression(prefix, expr.as_ref()),
+        Expression::InfixExpression(expr1, infix, expr2) => {
+            let ex_obj1 = eval_expression(expr1)?;
+            let ex_obj2 = eval_expression(expr2)?;
+            eval_infix_expression(infix, &ex_obj1, &ex_obj2)
+        }
         _ => Ok(Object::Null),
     }
 }
@@ -44,6 +49,25 @@ fn eval_bang_operator(right: &Object) -> Result<Object, String> {
 fn eval_minus_operator(right: &Object) -> Result<Object, String> {
     match right {
         Object::Integer(int) => Ok(Object::Integer(-(*int))),
+        _ => Ok(Object::Null),
+    }
+}
+
+fn eval_infix_expression(infix: &Infix, left: &Object, right: &Object) -> Result<Object, String> {
+    match (left, right) {
+        (Object::Integer(value1), Object::Integer(value2)) => {
+            eval_integer_infix_expression(infix, *value1, *value2)
+        }
+        _ => Ok(Object::Null),
+    }
+}
+
+fn eval_integer_infix_expression(infix: &Infix, left: i64, right: i64) -> Result<Object, String> {
+    match infix {
+        Infix::Plus => Ok(Object::Integer(left + right)),
+        Infix::Minus => Ok(Object::Integer(left - right)),
+        Infix::Asterisk => Ok(Object::Integer(left * right)),
+        Infix::Slash => Ok(Object::Integer(left / right)),
         _ => Ok(Object::Null),
     }
 }
@@ -124,6 +148,35 @@ mod tests {
 
             // THEN
             assert_eq!(result.unwrap().to_string(), expected);
+        }
+    }
+
+    #[test]
+    fn eval_integer_expression() {
+        // GIVEN
+        let tests = vec![
+            ("5", 5),
+            ("10", 10),
+            ("-5", -5),
+            ("-10", -10),
+            ("5 + 5 + 5 + 5 - 10", 10),
+            ("2 * 2 * 2 * 2 * 2", 32),
+            ("-50 + 100 + -50", 0),
+            ("5 * 2 + 10", 20),
+            ("5 + 2 * 10", 25),
+            ("20 + 2 * -10", 0),
+            ("50 / 2 * 2 + 10", 60),
+            ("2 * (5 + 10)", 30),
+            ("3 * 3 * 3 + 10", 37),
+            ("3 * (3 * 3) + 10", 37),
+            ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50),
+        ];
+        // WHEN
+        for (input, expected) in tests {
+            let result = test_eval(input);
+
+            // THEN
+            assert_eq!(result.unwrap().to_string(), expected.to_string());
         }
     }
 }
