c23febd5d4e77c189c7d448c08d2537c86f03f08
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-04-21 09:05:00 +0100

    add mut reference of Environemnt to the function object

--- src/object.rs
+++ src/object.rs
@@ -1,19 +1,20 @@
 use crate::ast::BlockStatement;
+use crate::environment::Environment;
 use std::fmt;
 use std::fmt::{write, Formatter};
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Object {
+pub enum Object<'a> {
     String(String),
     Integer(i64),
     Boolean(bool),
     Null,
-    Return(Box<Object>),
-    Function(Vec<String>, BlockStatement),
+    Return(Box<Object<'a>>),
+    Function(Vec<String>, BlockStatement, &'a Environment<'a>),
 }
 
 #[allow(unused_must_use)]
-impl fmt::Display for Object {
+impl<'a> fmt::Display for Object<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             Self::String(value) => write!(f, "{}", value),
@@ -21,13 +22,15 @@ impl fmt::Display for Object {
             Self::Boolean(value) => write!(f, "{}", value),
             Self::Return(value) => write!(f, "{}", *value),
             Self::Null => write!(f, "Null"),
-            Self::Function(params, body) => write!(f, "fn({}) {{ {} }}", params.join(", "), body),
+            Self::Function(params, body, _) => {
+                write!(f, "fn({}) {{ {} }}", params.join(", "), body)
+            }
         };
         Ok(())
     }
 }
 
-impl Object {
+impl<'a> Object<'a> {
     pub fn is_truthy(&self) -> bool {
         match self {
             Self::Null => false,
@@ -44,7 +47,7 @@ impl Object {
             Self::Boolean(_) => "BOOLEAN",
             Self::Null => "NULL",
             Self::Return(_) => "RETURN",
-            Self::Function(_, _) => "FUNCTION",
+            Self::Function(_, _, _) => "FUNCTION",
         }
     }
 }
