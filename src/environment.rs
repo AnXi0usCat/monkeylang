eae3a1f73cc6d25335fb487ba640107fc97047d9
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-04-21 09:04:18 +0100

    implement derivable traits on environment and add lifetimer params for the Object

--- src/environment.rs
+++ src/environment.rs
@@ -1,22 +1,23 @@
 use crate::object::Object;
 use std::collections::HashMap;
 
-pub struct Environment {
-    store: HashMap<String, Object>,
+#[derive(Debug, PartialEq, Clone)]
+pub struct Environment<'a> {
+    store: HashMap<String, Object<'a>>,
 }
 
-impl Environment {
+impl<'a> Environment<'a> {
     pub fn new() -> Self {
         Environment {
             store: HashMap::new(),
         }
     }
 
-    pub fn set(&mut self, k: &str, v: Object) {
+    pub fn set(&mut self, k: &str, v: Object<'a>) {
         self.store.insert(k.to_string(), v);
     }
 
-    pub fn get(&self, k: &str) -> Option<&Object> {
+    pub fn get(&self, k: &str) -> Option<&Object<'a>> {
         self.store.get(k)
     }
 }
