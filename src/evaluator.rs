1e4bd85bb3f4a02c9114f9554398777a61c6a331
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-04-22 18:32:46 +0100

    unit test for function evaluation

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -445,7 +445,28 @@ mod tests {
         // WHEN
         for (input, expected) in tests {
             let result = test_eval(input);
+            // THEN
+            assert_eq!(result.unwrap().to_string(), expected);
+        }
+    }
 
+    #[test]
+    fn eval_function_application() {
+        // GIVEN
+        let tests = vec![
+            ("let identity = fn(x) { x; }; identity(5);", "5"),
+            ("let identity = fn(x) { return x; }; identity(5);", "5"),
+            ("let double = fn(x) { x * 2; }; double(5);", "10"),
+            ("let add = fn(x, y) { x + y; }; add(5, 5);", "10"),
+            (
+                "let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));",
+                "20",
+            ),
+            ("fn(x) { x; }(5)", "5"),
+        ];
+        // WHEN
+        for (input, expected) in tests {
+            let result = test_eval(input);
             // THEN
             assert_eq!(result.unwrap().to_string(), expected);
         }
