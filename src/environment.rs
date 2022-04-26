cb2085f93023542e5f6765c2cda812e8c271a6a9
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-04-26 18:58:22 +0100

    extend the environment with environmet of the outer scope

--- src/environment.rs
+++ src/environment.rs
@@ -1,15 +1,28 @@
 use crate::object::Object;
+use std::borrow::Borrow;
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::ops::Deref;
+use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Environment {
     store: HashMap<String, Object>,
+    outer: Option<Rc<RefCell<Environment>>>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Environment {
             store: HashMap::new(),
+            outer: None,
+        }
+    }
+
+    pub fn extend(outer: Rc<RefCell<Self>>) -> Environment {
+        Environment {
+            store: HashMap::new(),
+            outer: Some(outer),
         }
     }
 
@@ -17,7 +30,13 @@ impl Environment {
         self.store.insert(k.to_string(), v);
     }
 
-    pub fn get(&self, k: &str) -> Option<&Object> {
-        self.store.get(k)
+    pub fn get(&self, k: &str) -> Option<Object> {
+        match self.store.get(k) {
+            Some(value) => Some(value.clone()),
+            None => self
+                .outer
+                .as_ref()
+                .and_then(|obj| obj.deref().borrow().get(k)),
+        }
     }
 }
