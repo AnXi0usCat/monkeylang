ee71531a23436035e54a1a1ac5c404b5087736d5
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2022-05-28 20:52:06 +0100

    Object representation for arrays

--- src/object.rs
+++ src/object.rs
@@ -8,6 +8,7 @@ use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Object {
+    Array(Vec<Object>),
     String(String),
     Integer(i64),
     Boolean(bool),
@@ -21,6 +22,15 @@ pub enum Object {
 impl fmt::Display for Object {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
+            Self::Array(contents) => write!(
+                f,
+                "{}",
+                contents
+                    .into_iter()
+                    .map(|v| v.to_string())
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            ),
             Self::String(value) => write!(f, "{}", value),
             Self::Integer(value) => write!(f, "{}", value),
             Self::Boolean(value) => write!(f, "{}", value),
@@ -47,6 +57,7 @@ impl Object {
 
     pub fn obj_type(&self) -> &str {
         match self {
+            Self::Array(_) => "ARRAY",
             Self::Integer(_) => "INTEGER",
             Self::String(_) => "STRING",
             Self::Boolean(_) => "BOOLEAN",
